6d6ab96f53999c891e67d58b28ed02e086ca8919
maratoro <sarko.zxc.bluagh@gmail.com>
2023-11-07 06:47:38 +0000

    uu

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const numeroFibonacci = parse(int(input("Intruduzca la cantidad de numeros de la sucesión de Fibonacci que desea ver: ")));
+const numeroFibonacci = parseInt(input("Intruduzca la cantidad de numeros de la sucesión de Fibonacci que desea ver: "));
 
-if (numeroFibonacci(isNaN)){
-console.log("Introduzca un numero: ")
- } else {
-console.log(Fibonacci);
- }
-function Fibonacci(numeroFibonacci) {
- const fib = [ ];
+if (isNaN(numeroFibonacci)){
+    console.log("Introduzca un numero: ")
+} else {
+    function Fibonacci(numeroFibonacci) {
+        const fib = [0, 1];
 
-  for (let i = 2; i <= numeroFibonacci; i++) {
-    fib[i] = fib[i-1] + fib[i-2];
-   fib.push(fib);  
-}}
+        for (let i = 2; i <= numeroFibonacci; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+
+        return fib;
+    }
+
+    console.log(Fibonacci(numeroFibonacci)); 
+}
 /*quiero que almacene el resultado de cada iteracion en un array y lo muestre en pantalla cuando llegue a la cifra que introdujo el user,, como hago esooooooooo aaa*/ 
-
-console.log(Fibonacci(fib));
